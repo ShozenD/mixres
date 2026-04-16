0b88f16d021064d8e324682cf0e9f2dc1e05b65d
Shozen Dan <shozen.dan@keio.jp>
2026-04-16 15:45:43 +0100

    [update] reorganize repository

--- aggpp/sim/__init__.py
+++
@@ -1,8 +0,0 @@
-from ._generate_data import generate_data
-from ._aggregate import assign_disjoint_bins, assign_overlapping_bins
-
-__all__ = [
-  'generate_data',
-  'assign_disjoint_bins',
-  'assign_overlapping_bins'
-]
